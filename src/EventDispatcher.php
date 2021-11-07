ffade43d19b80b5470b058bc00abdc668729e2a7
lev <lev@vinylcoding.ru>
2021-11-07 10:12:25 +0000

    add implementation

--- src/EventDispatcher.php
+++ src/EventDispatcher.php
@@ -1,6 +1,31 @@
 <?php
 
-class EventDispatcher
-{
+namespace CodingLiki\EventDispatcher;
 
+use CodingLiki\PsrEventDispatcher\EventDispatcherInterface;
+use CodingLiki\PsrEventDispatcher\ListenerProviderInterface;
+use CodingLiki\PsrEventDispatcher\StoppableEventInterface;
+
+class EventDispatcher implements EventDispatcherInterface
+{
+    /**
+     * @param ListenerProviderInterface[] $listenerProviders
+     */
+    public function __construct(private array $listenerProviders)
+    {
+    }
+
+    public function dispatch(\object $event): \object
+    {
+        foreach ($this->listenerProviders as $listenerProvider) {
+            foreach ($listenerProvider->getListenersForEvent($event) as $listener) {
+                if ($event instanceof StoppableEventInterface && $event->isPropagationStopped()) {
+                    return $event;
+                }
+                call_user_func($listener, $event);
+            }
+        }
+
+        return $event;
+    }
 }
---
+++ src/ListenerProviders/ArrayListenerProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace CodingLiki\EventDispatcher\ListenerProviders;
+
+use CodingLiki\PsrEventDispatcher\ListenerProviderInterface;
+
+class ArrayListenerProvider implements ListenerProviderInterface
+{
+    /**
+     * @param array<string, array<callable>> $listeners
+     */
+    public function __construct(private array $listeners = [])
+    {
+    }
+
+    public function addListener(string $eventClass, callable $listener): static
+    {
+        isset($this->listeners[$eventClass]) ?: $this->listeners[$eventClass] = [];
+        $this->listeners[$eventClass][] = $listener;
+
+        return $this;
+    }
+
+    public function getListenersForEvent(object $event): iterable
+    {
+        return $this->listeners[$event::class] ?? [];
+    }
+}
